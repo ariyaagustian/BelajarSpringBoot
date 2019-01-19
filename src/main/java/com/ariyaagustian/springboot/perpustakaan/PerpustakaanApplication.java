98543b0de23dac34dae47d0bc89947dea43177cc
ariyaagustian <ariyaagustian@gmail.com>
2019-01-19 14:32:12 +0700

    Update Oauth2

--- src/main/java/com/ariyaagustian/springboot/perpustakaan/PerpustakaanApplication.java
+++ src/main/java/com/ariyaagustian/springboot/perpustakaan/PerpustakaanApplication.java
@@ -2,9 +2,16 @@ package com.ariyaagustian.springboot.perpustakaan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class PerpustakaanApplication {
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(11);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PerpustakaanApplication.class, args);
--- src/main/java/com/ariyaagustian/springboot/perpustakaan/config/WebSecurityConfig.java
+++ src/main/java/com/ariyaagustian/springboot/perpustakaan/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -34,16 +35,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(
                         "/swagger-ui.html",
                         "/swagger-resources/**",
-                        "/v2/api-docs").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
+                        "/v2/api-docs",
+                        "/oauth/**").permitAll()
+                .anyRequest().authenticated();
     }
 
-
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(11);
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
 
